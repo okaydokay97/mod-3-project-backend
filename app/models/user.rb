594eaa4ff6916d0e6830e38838c2d96f4bc81250
okaydokay97 <rubens.fernandes97@gmail.com>
2020-07-08 10:25:38 -0500

    working on models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
-  has_many :matches
-  has_many :rejects 
+  has_many :matches, :class_name => "Match", :foreign_key => :user_one_id
+  has_many :user_ones, through: :matches
+  has_many :matches, :class_name => "Match", :foreign_key => :user_two_id
+  has_many :user_twos, through: :matches
+  # has_many :rejects, :foreign_key => "user_one"
+  # has_many :rejects, :foreign_key => "user_two"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,15 +17,4 @@ age = Faker::Number.between(from: 18, to: 70)
 gender = Faker::Gender.binary_type
 img_url = Faker::LoremFlickr.image(size: "300x300", search_terms: ["#{gender}"])
 
-u1 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-u2 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-u3 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-u4 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-u5 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-u6 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-u7 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-u8 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-u9 = User.create(name: name, email: email, bio: bio_quote, gender: gender, age: age, img_url: img_url)
-
-
-m1
+10.times{ User.create(name: Faker::Name.first_name, email: Faker::Internet.email, bio: Faker::Quote.famous_last_words, gender: Faker::Gender.binary_type, age: Faker::Number.between(from: 18, to: 70), img_url: Faker::LoremFlickr.image(size: "300x300", search_terms: ["#{Faker::Gender.binary_type}"])) }
