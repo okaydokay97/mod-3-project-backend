b2ab387d242384cf9a2ccdf508226d6a01d3e4b6
okaydokay97 <rubens.fernandes97@gmail.com>
2020-07-08 11:10:34 -0500

    created associations

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,4 @@
 class Match < ApplicationRecord
-  belongs_to :user_one, :foreign_key => :user_two_id
-  belongs_to :user_two, :foreign_key => :user_one_id
-  # belongs_to :user_one, :class_name => "User"
-  # belongs_to :user_two, :class_name => "User"
+  belongs_to :user
+  belongs_to :user_two, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  has_many :matches, :class_name => "Match", :foreign_key => :user_one_id
-  has_many :user_ones, through: :matches
-  has_many :matches, :class_name => "Match", :foreign_key => :user_two_id
+  has_many :matches, dependent: :destroy
   has_many :user_twos, through: :matches
-  # has_many :rejects, :foreign_key => "user_one"
-  # has_many :rejects, :foreign_key => "user_two"
+
+  has_many :rejects, dependent: :destroy
+  has_many :user_twos, through: :rejects
 end
