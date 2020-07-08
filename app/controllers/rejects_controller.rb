bc94608345757f605851d509e8dd78933850d498
Yann233 <yannxyang@sina.com>
2020-07-08 11:47:23 -0500

    Controller+questions

--- app/controllers/rejects_controller.rb
+++ app/controllers/rejects_controller.rb
@@ -1,2 +1,17 @@
 class RejectsController < ApplicationController
+  def index
+    rejects = Reject.all
+    render json: rejects
+  end
+
+  def create
+    @match = Reject.create(reject_params)
+    render json: @reject
+  end
+  
+  private
+  def match_params
+      params.require(:reject).permit(:user, :user_two)
+  end
+
 end
